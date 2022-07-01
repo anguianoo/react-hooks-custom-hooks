8a05ac23496276eb73a22a3500a2d4d593a39268
anguianoo <njanguiano@gmail.com>
2022-06-30 20:31:27 -0700

    done

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { makeEmojiList } from "../utils";
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { makeEmojiList } from "../utils"
 
 function ArticlePage() {
   // fetch data for a post
-  const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
+  const { id } = useParams()
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [post, setPost] = useState(null)
 
-  const url = `http://localhost:4000/posts/${id}`;
+  const url = `http://localhost:4000/posts/${id}`
   useEffect(() => {
-    setIsLoaded(false);
+    setIsLoaded(false)
     fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
+      .then(r => r.json())
+      .then(post => {
+        setPost(post)
+        setIsLoaded(true)
+      })
+  }, [url])
 
   // set the document title
-  const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
+  const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted"
   useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
+    document.title = pageTitle
+  }, [pageTitle])
 
-  if (!isLoaded) return <h3>Loading...</h3>;
+  if (!isLoaded) return <h3>Loading...</h3>
 
-  const { minutes, title, date, preview } = post;
-  const emojis = makeEmojiList(minutes);
+  const { minutes, title, date, preview } = post
+  const emojis = makeEmojiList(minutes)
 
   return (
     <article>
@@ -38,7 +38,7 @@ function ArticlePage() {
       </small>
       <p>{preview}</p>
     </article>
-  );
+  )
 }
 
-export default ArticlePage;
+export default ArticlePage
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,33 +1,35 @@
-import React, { useEffect, useState } from "react";
-import About from "./About";
-import ArticleList from "./ArticleList";
+import React, { useEffect, useState } from "react"
+import About from "./About"
+import ArticleList from "./ArticleList"
+import useDocumentTitle from "../hooks/useDocumentTitle";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    setIsLoaded(false);
+    setIsLoaded(false)
     fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+      .then(r => r.json())
+      .then(posts => {
+        setPosts(posts)
+        setIsLoaded(true)
+      })
+  }, [])
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+  useDocumentTitle()
+  // useEffect(() => {
+  //   document.title = "Underreacted | Home";
+  // }, []);
 
   return (
     <>
       <About />
       {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
     </>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
